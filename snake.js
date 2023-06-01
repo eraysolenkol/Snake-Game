41f41d2b36e54d927c90dd475244d52ecd738026
Eray <solenkoleray@gmail.com>
2023-06-01 16:14:23 +0300

    bug fixes
    bug fixes

--- snake.js
+++ snake.js
@@ -4,13 +4,42 @@ class SnakeGame {
         this.ctx = ctx;
         setInterval(this.loop.bind(this), 100);
         document.addEventListener('keydown', this.onPressKey.bind(this));
-        this.scoreboard = document.getElementById("scoreboard"); 
-        this.foodUrl = "apple.png";
-        this.snakeHeadUrl = "snake_head.png";
-        this.snakeBodyUrl = "snake_body.png";
+        this.scoreboard = document.getElementById("scoreboard");
+        this.foodUrl = "images/apple.png";
+        this.snakeHeadUrl = "images/snake_head.png";
+        this.snakeBodyUrl = "images/snake_body.png";
     }
 
-    init() {
+
+    loadImages() {
+        const imagePromises = [];
+
+        this.snakeHeadImage = new Image();
+        const snakeHeadPromise = new Promise((resolve) => {
+            this.snakeHeadImage.onload = resolve;
+        });
+        this.snakeHeadImage.src = this.snakeHeadUrl;
+        imagePromises.push(snakeHeadPromise);
+
+        this.snakeBodyImage = new Image();
+        const snakeBodyPromise = new Promise((resolve) => {
+            this.snakeBodyImage.onload = resolve;
+        });
+        this.snakeBodyImage.src = this.snakeBodyUrl;
+        imagePromises.push(snakeBodyPromise);
+
+        this.foodImage = new Image();
+        const foodPromise = new Promise((resolve) => {
+            this.foodImage.onload = resolve;
+        });
+        this.foodImage.src = this.foodUrl;
+        imagePromises.push(foodPromise);
+
+        return Promise.all(imagePromises);
+    }
+
+    async init() {
+        await this.loadImages();
         this.snake = [];
         this.cellSize = 20;
         this.food = {
@@ -27,12 +56,6 @@ class SnakeGame {
             y: 0
         };
         this.updateScoreboard();
-        this.snakeHeadImage = new Image();
-        this.snakeHeadImage.src = this.snakeHeadUrl;
-        this.snakeBodyImage = new Image();
-        this.snakeBodyImage.src = this.snakeBodyUrl;
-        this.foodImage = new Image();
-        this.foodImage.src = this.foodUrl;
     }
 
     loop() {
@@ -41,13 +64,13 @@ class SnakeGame {
     }
 
     onPressKey(e) {
-        switch(e.key) {
+        switch (e.key) {
             case 'w':
             case 'ArrowUp':
                 this.velocity.x = 0;
                 this.velocity.y = -1;
                 break;
-            case 's': 
+            case 's':
             case 'ArrowDown':
                 this.velocity.x = 0;
                 this.velocity.y = 1;
@@ -69,9 +92,9 @@ class SnakeGame {
 
 
     update() {
-        for (let i = this.snake.length-1; i > 0; i--) {
-            this.snake[i].x = this.snake[i-1].x;
-            this.snake[i].y = this.snake[i-1].y;
+        for (let i = this.snake.length - 1; i > 0; i--) {
+            this.snake[i].x = this.snake[i - 1].x;
+            this.snake[i].y = this.snake[i - 1].y;
         }
         this.snake[0].x += this.velocity.x * this.cellSize;
         this.snake[0].y += this.velocity.y * this.cellSize;
@@ -82,7 +105,7 @@ class SnakeGame {
         this.leaderboard = localStorage.getItem("leaderboard");
         this.leaderboardJSON = JSON.parse(this.leaderboard);
         this.scoreboard.innerHTML = "";
-        this.leaderboardJSON.sort((a,b) => b.score - a.score);
+        this.leaderboardJSON.sort((a, b) => b.score - a.score);
         const size = (this.leaderboardJSON.length >= 5) ? 5 : this.leaderboardJSON.length;
         for (let i = 0; i < size; i++) {
             let div = document.createElement("div");
@@ -103,7 +126,7 @@ class SnakeGame {
         } else {
             leaderboardJSON = JSON.parse(leaderboard);
         }
-        leaderboardJSON.push({name: name, score: this.score});
+        leaderboardJSON.push({ name: name, score: this.score });
         localStorage.setItem("leaderboard", JSON.stringify(leaderboardJSON));
         this.updateScoreboard();
         this.init();
@@ -120,23 +143,23 @@ class SnakeGame {
     draw() {
         // drawing background
         this.ctx.fillStyle = '#000000';
-        this.ctx.fillRect(0,0, this.canvas.width, this.canvas.height);
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         // drawing food
-        this.ctx.drawImage(this.foodImage, this.food.x, this.food.y , this.cellSize, this.cellSize);
+        this.ctx.drawImage(this.foodImage, this.food.x, this.food.y, this.cellSize, this.cellSize);
         //drawing snake
         for (let i = 0; i < this.snake.length; i++) {
             if (i === 0) {
                 const angle = Math.atan2(this.velocity.y, this.velocity.x);
                 this.rotateAndDrawImage(this.snakeHeadImage, this.snake[i].x, this.snake[i].y, angle);
             } else {
-                this.ctx.drawImage(this.snakeBodyImage, this.snake[i].x, this.snake[i].y , this.cellSize -2, this.cellSize-2);
+                this.ctx.drawImage(this.snakeBodyImage, this.snake[i].x, this.snake[i].y, this.cellSize - 2, this.cellSize - 2);
             }
         }
         //drawing score text
         this.ctx.fillStyle = '#00ff00';
         this.ctx.font = '20px sarif';
-        this.ctx.fillText(`Score: ${this.score}`, 5,20); 
-        
+        this.ctx.fillText(`Score: ${this.score}`, 5, 20);
+
     }
 
     createRandomFood() {
@@ -201,11 +224,11 @@ class SnakeGame {
         // wall collision
         if (this.snake[0].x < 0) {
             this.snake[0].x = this.canvas.width - this.cellSize;
-        } else if (this.snake[0].x >= this.canvas.width) { 
+        } else if (this.snake[0].x >= this.canvas.width) {
             this.snake[0].x = 0;
         } else if (this.snake[0].y < 0) {
             this.snake[0].y = this.canvas.height - this.cellSize;
-        } else if (this.snake[0].y >= this.canvas.height) { 
+        } else if (this.snake[0].y >= this.canvas.height) {
             this.snake[0].y = 0;
         }
     }
